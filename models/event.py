f2f097ad38dad5e506737f8b6ebe2ed219f17db5
Michal Mikolajczyk <mikolajczyk01@gmail.com>
2024-02-05 20:20:15 +0100

    protection drawing yourself in last round

--- models/event.py
+++ models/event.py
@@ -26,21 +26,33 @@ class Event:
         return new_player
 
     def draw_name_for(self, player: Player):
-        # More complex problem
-        # What if last drawing person can draw only himself?
-
         if player.drawn_name is None:
-            draw_choice = random.choice(self.drawing_bucket)
+            remaining_names = [p for p in self.drawing_bucket if p != player]
+            if len(remaining_names) == 2:
+                only_choice = []
+                participants_left = []
+                for p in self.participants:
+                    if p.drawn_name is None:
+                        participants_left.append(p)
+                for n in remaining_names:
+                    if n in participants_left:
+                        only_choice.append(n)
+                if only_choice:
+                    player.drawn_name = only_choice[0]
+                    self.drawing_bucket.remove(player.drawn_name)
+                else:
+                    player.drawn_name = random.choice(remaining_names)
+                    self.drawing_bucket.remove(player.drawn_name)
 
-            if draw_choice == player:
-                try:
-                    self.draw_name_for(player)
-                except RecursionError:  # In this case there is only mayself to draw
-                    print("Nobody else to draw but yourself")
-                    self.drawing_bucket.remove(draw_choice)
-                    player.drawn_name = draw_choice
+            elif not remaining_names:
+                print("[INFO] No one left but you")
+                player.drawn_name = player
             else:
-                self.drawing_bucket.remove(draw_choice)
-                player.drawn_name = draw_choice
+                player.drawn_name = random.choice(remaining_names)
+                self.drawing_bucket.remove(player.drawn_name)
         else:
-            print("Already made a draw")
+            pass
+
+    def reset_draw_for(self, player: Player):
+        self.drawing_bucket.append(player.drawn_name)
+        player.drawn_name = None
