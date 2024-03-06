5b7cb2f620d8a6fbd877c27ea6075a9fa5949dcc
Michal Mikolajczyk <mikolajczyk01@gmail.com>
2024-03-06 22:27:29 +0100

    typo fix

--- manual_tests.py
+++ manual_tests.py
@@ -1,13 +1,11 @@
 from secret_santa.event_manager import EventManager
-from secret_santa.models.models import Event, User
+from secret_santa.models.models import Event
 
 MAIN_PARTY = "Christmas 2023"
 
 christmas_party = Event(name=MAIN_PARTY)
 event_manager = EventManager(christmas_party)
 
-
-event_manager.assign_user_
 
 mic = event_manager.add_participant("mic")
 ania = event_manager.add_participant("Ania")
